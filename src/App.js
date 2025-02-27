2b0c3ce759a07ffda116ba22c2de646bcf169a86
danbaulk <danielbaulk@gmail.com>
2025-02-27 17:07:03 +0000

    remove or

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import fplLogo from './assets/fpl-logo.png';
 import { useState, useEffect } from 'react';
 
-const API_URL = process.env.REACT_APP_GO_API || 'http://localhost:8081';
+const API_URL = process.env.REACT_APP_GO_API;
 
 function App() {
   const [gameweek, setGameweek] = useState(null);
